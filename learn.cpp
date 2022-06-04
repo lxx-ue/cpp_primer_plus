727fc69db4502100e35e2c4baeffde0e1e2567f0
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-06-04 21:47:03 +0300

    Queue realization

--- learn.cpp
+++ learn.cpp
@@ -1293,41 +1293,44 @@ int main()
 	//cout << *top;
 
 	// #4
-	Stack1 st;
-	char ch;
-	unsigned long po;
-	cout << "Please enter A to add a purchase,\n"
-		<< "P to process a PO, or Q to quit.\n";
-	while (cin >> ch && toupper(ch) != 'Q')
-	{
-		while (cin.get() != '\n')
-			continue;
-		if (!isalpha(ch))
-		{
-			cout << '\a';
-			continue;
-		}
-		switch (toupper(ch))
-		{
-		case 'A':cout << "Enter a PO number to add: ";
-			cin >> po;
-			if (st.is_full())
-				cout << "stack already full\n";
-			else st.push(po);
-			break;
-		case 'P':
-			if (st.is_empty())
-				cout << "stack already empty\n";
-			else {
-				st.pop(po);
-				cout << "PO #" << po << " popped\n";
-			}
-			break;
-		}
-		cout << "Please enter A to add a purchase order,\n"
-			<< "P to process a PO, or Q to quit.\n";
-	}
-	cout << "Bуе\n";
+	//Stack1 st;
+	//char ch;
+	//unsigned long po;
+	//cout << "Please enter A to add a purchase,\n"
+	//	<< "P to process a PO, or Q to quit.\n";
+	//while (cin >> ch && toupper(ch) != 'Q')
+	//{
+	//	while (cin.get() != '\n')
+	//		continue;
+	//	if (!isalpha(ch))
+	//	{
+	//		cout << '\a';
+	//		continue;
+	//	}
+	//	switch (toupper(ch))
+	//	{
+	//	case 'A':cout << "Enter a PO number to add: ";
+	//		cin >> po;
+	//		if (st.is_full())
+	//			cout << "stack already full\n";
+	//		else st.push(po);
+	//		break;
+	//	case 'P':
+	//		if (st.is_empty())
+	//			cout << "stack already empty\n";
+	//		else {
+	//			st.pop(po);
+	//			cout << "PO #" << po << " popped\n";
+	//		}
+	//		break;
+	//	}
+	//	cout << "Please enter A to add a purchase order,\n"
+	//		<< "P to process a PO, or Q to quit.\n";
+	//}
+	//cout << "Bуе\n";
+
+	// #5
+	
 
 #pragma endregion
 }
