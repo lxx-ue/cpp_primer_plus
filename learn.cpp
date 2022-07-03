8e8bbeb6d3122e669b7e233693fe693a04ff3906
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-07-03 21:45:53 +0300

    task 4 chapter 15, chapter 15 finished

--- learn.cpp
+++ learn.cpp
@@ -1888,25 +1888,86 @@ int main()
 	//grey.view_mode();
 
 	// #2-3
-	double x, y, z;
-	cout << "Enter two numbers: ";
-	while (cin >> x >> y)
+	//double x, y, z;
+	//cout << "Enter two numbers: ";
+	//while (cin >> x >> y)
+	//{
+	//	try
+	//	{
+	//		z = hmean(x, y);
+	//		cout << "Harmonic mean of " << x << " and " << y
+	//			<< " is " << z << endl;
+	//		cout << "Geometric mean of " << x << " and " << y
+	//			<< " is " << gmean(x,y) << endl;
+	//		cout << "Enter next set of numbers <q to quit>: ";
+	//	}
+	//	catch (base_exc& hg)
+	//	{
+	//		cout << hg.what();
+	//		break;
+	//	}
+	//}
+
+	// #4
+	double vals1[12] =
 	{
-		try
+		1220, 1100, 1122, 2212, 1232, 2334,
+		2884, 2393, 3302, 2922, 3002, 3544
+	};
+	double vals2[12] =
+	{
+		12, 11, 22, 21, 32, 34,
+		28, 29, 33, 29, 32, 35
+	};
+	Sales sales1(2011, vals1, 12);
+	LabeledSales sales2("Blogstar", 2012, vals2, 12);
+	cout << "First try block: \n";
+	try {
+		int i;
+		cout << "Year = " << sales1.Year() << endl;
+		for (i = 0; i < 12; ++i)
 		{
-			z = hmean(x, y);
-			cout << "Harmonic mean of " << x << " and " << y
-				<< " is " << z << endl;
-			cout << "Geometric mean of " << x << " and " << y
-				<< " is " << gmean(x,y) << endl;
-			cout << "Enter next set of numbers <q to quit>: ";
+			cout << sales1[i] << " ";
+			if (i % 6 == 5)
+				cout << endl;
 		}
-		catch (base_exc& hg)
+		cout << "Year = " << sales2.Year() << endl;
+		cout << "Label = " << sales2.Label() << endl;
+		for (i = 0; i <= 12; ++i)
 		{
-			cout << hg.what();
-			break;
+			cout << sales2[i] << " ";
+			if (i % 6 == 5)
+				cout << endl;
 		}
+		cout << "End of try block1.\n";
 	}
+	catch (Sales::bad_index& bad)
+	{
+		cout << bad.what();
+		if (typeid(LabeledSales::nbad_index&) == typeid(bad))
+		{
+			LabeledSales::nbad_index& nbi = dynamic_cast<LabeledSales::nbad_index&>(bad);
+			cout << "Company: " << nbi.label_val() << endl;
+		}
+		cout << "bad_index: " << bad.bi_val() << endl;
+	}
+	cout << "\nNext try block:\n";
+	try {
+		sales2[2] = 37.5;
+		sales1[20] = 23345;
+		cout << "End of try block2.\n";
+	}
+	catch (Sales::bad_index& bad)
+	{
+		cout << bad.what();
+		if (typeid(LabeledSales::nbad_index&) == typeid(bad))
+		{
+			LabeledSales::nbad_index& nbi = dynamic_cast<LabeledSales::nbad_index&>(bad);
+			cout << "Company: " << nbi.label_val() << endl;
+		}
+		cout << "bad index: " << bad.bi_val() << endl;
+	}
+	cout << "Done\n";
 #pragma endregion
 }
 
