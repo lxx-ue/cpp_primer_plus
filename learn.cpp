68483f638cd87af7c3173c3eaf857295e2c22032
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-07-12 19:58:14 +0300

    chapter 16 tasks 1-2

--- learn.cpp
+++ learn.cpp
@@ -473,6 +473,10 @@ const string wordlist[NUM] = { "apiary", "beetle", "cereal",
 	"jackal", "keeper", "loaner", "manage", "nonce", "onset",
 	"plaid", "quilt", "remote", "stolid", "train", "useful",
 	"valid", "whence", "xenon", "yearn", "zippy" };
+
+// #1
+bool check_palindrome(const string&);
+void clean_string(string&);
 #pragma endregion
 
 int main()
@@ -1979,70 +1983,77 @@ int main()
 #pragma endregion
 
 #pragma region chapter16
+	// word game
+	//std::srand(std::time(0));
+	//char play;
+	//cout << "Will you play a word game? <y/n> ";
+	//cin >> play;
+	//play = tolower(play);
+	//while (play == 'y')
+	//{
+	//	string target = wordlist[std::rand() % NUM];
+	//	int length = target.length();
+	//	string attempt(length, '-');
+	//	string badchars;
+	//	int guesses = 6;
+	//	cout << "Guess my secret word. It has " << length
+	//		<< " letters, and you guess\none letter at time."
+	//		<< " You get " << guesses
+	//		<< " wrong guesses.\n";
+	//	cout << "Your word: " << attempt << endl;
+	//	while (guesses > 0 && attempt != target)
+	//	{
+	//		char letter;
+	//		cout << "Guess a letter: ";
+	//		cin >> letter;
+	//		if (badchars.find(letter) != string::npos
+	//			|| attempt.find(letter) != string::npos)
+	//		{
+	//			cout << "You already guessed that. Try again.\n";
+	//			continue;
+	//		}
+	//		int loc = target.find(letter);
+	//		if (loc == string::npos)
+	//		{
+	//			cout << "Oh, bad guess!\n";
+	//			guesses--;
+	//			badchars += letter;
+	//		}
+	//		else
+	//		{
+	//			cout << "Good guess!\n";
+	//			attempt[loc] = letter;
+	//			//проверить не появляется ли буква еще раз
+	//			loc = target.find(letter, loc + 1);
+	//			while (loc != string::npos) {
+	//				attempt[loc] = letter;
+	//				loc = target.find(letter, loc + 1);
+	//			}
+	//		}
+	//		cout << "You word: " << attempt << endl;
+	//		if (attempt != target)
+	//		{
+	//			if (badchars.length() > 0)
+	//				cout << "Bad choices: " << badchars << endl;
+	//			cout << guesses << " bad guesses left\n";
+	//		}
+	//	}
+	//	if (guesses > 0)
+	//		cout << "That's right!\n";
+	//	else
+	//		cout << "Sorry, the word is " << target << ".\n";
+	//	cout << "Will you play another? <y/n> ";
+	//	cin >> play;
+	//	play = tolower(play);
+	//}
 
-std::srand(std::time(0));
-char play;
-cout << "Will you play a word game? <y/n> ";
-cin >> play;
-play = tolower(play);
-while (play == 'y')
-{
-	string target = wordlist[std::rand() % NUM];
-	int length = target.length();
-	string attempt(length, '-');
-	string badchars;
-	int guesses = 6;
-	cout << "Guess my secret word. It has " << length
-		<< " letters, and you guess\none letter at time."
-		<< " You get " << guesses
-		<< " wrong guesses.\n";
-	cout << "Your word: " << attempt << endl;
-	while (guesses > 0 && attempt != target)
-	{
-		char letter;
-		cout << "Guess a letter: ";
-		cin >> letter;
-		if (badchars.find(letter) != string::npos
-			|| attempt.find(letter) != string::npos)
-		{
-			cout << "You already guessed that. Try again.\n";
-			continue;
-		}
-		int loc = target.find(letter);
-		if (loc == string::npos)
-		{
-			cout << "Oh, bad guess!\n";
-			guesses--;
-			badchars += letter;
-		}
-		else
-		{
-			cout << "Good guess!\n";
-			attempt[loc] = letter;
-			//проверить не появляется ли буква еще раз
-			loc = target.find(letter, loc + 1);
-			while (loc != string::npos) {
-				attempt[loc] = letter;
-				loc = target.find(letter, loc + 1);
-			}
-		}
-		cout << "You word: " << attempt << endl;
-		if (attempt != target)
-		{
-			if (badchars.length() > 0)
-				cout << "Bad choices: " << badchars << endl;
-			cout << guesses << " bad guesses left\n";
-		}
-	}
-	if (guesses > 0)
-		cout << "That's right!\n";
-	else
-		cout << "Sorry, the word is " << target << ".\n";
-	cout << "Will you play another? <y/n> ";
-	cin >> play;
-	play = tolower(play);
-}
-
+	// #1-2 
+	string str;
+	getline(cin, str);
+	clean_string(str);
+	cout << "Cleaned string: " << str << endl;
+	if (check_palindrome(str)) cout << "it is a palindrome!";
+	else cout << "it is not a palindrome";
 #pragma endregion
 }
 
@@ -2472,5 +2483,24 @@ Grand* GetOne()
 		break;
 	}
 	return p;
+}
+#pragma endregion
+
+#pragma region func_ch16
+void clean_string(string& str)
+{
+	string newstr;
+	for (int i = 0; i < str.size(); i++)
+		if (isalpha(str[i]))
+			newstr.push_back(tolower(str[i]));
+	str = newstr;
+}
+bool check_palindrome(const string& str)
+{
+	if (str.size() == 0) return false;
+	for (int i = 0; i < str.size() / 2; i++)
+		if (str[i] != str[str.size() - i - 1])
+			return false;
+	return true;
 }
 #pragma endregion
