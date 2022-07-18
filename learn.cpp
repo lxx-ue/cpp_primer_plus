c48ddf7fe5a551dfe78ef23a5fdf3ceb1e889f67
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-07-18 20:22:50 +0300

    chapter 16 task 3

--- learn.cpp
+++ learn.cpp
@@ -2024,86 +2024,82 @@ int main()
 	//	cout << *si << ": " << wordmap[*si] << endl;
 
 	// #3
-	//std::srand(std::time(0));
-	//char play;
-	//cout << "Will you play a word game? <y/n> ";
-	//cin >> play;
-	//play = tolower(play);
-	//while (play == 'y')
-	//{
-	//	string target = wordlist[std::rand() % NUM];
-	//	int length = target.length();
-	//	string attempt(length, '-');
-	//	string badchars;
-	//	int guesses = 6;
-	//	cout << "Guess my secret word. It has " << length
-	//		<< " letters, and you guess\none letter at time."
-	//		<< " You get " << guesses
-	//		<< " wrong guesses.\n";
-	//	cout << "Your word: " << attempt << endl;
-	//	while (guesses > 0 && attempt != target)
-	//	{
-	//		char letter;
-	//		cout << "Guess a letter: ";
-	//		cin >> letter;
-	//		if (badchars.find(letter) != string::npos
-	//			|| attempt.find(letter) != string::npos)
-	//		{
-	//			cout << "You already guessed that. Try again.\n";
-	//			continue;
-	//		}
-	//		int loc = target.find(letter);
-	//		if (loc == string::npos)
-	//		{
-	//			cout << "Oh, bad guess!\n";
-	//			guesses--;
-	//			badchars += letter;
-	//		}
-	//		else
-	//		{
-	//			cout << "Good guess!\n";
-	//			attempt[loc] = letter;
-	//			//проверить не появляется ли буква еще раз
-	//			loc = target.find(letter, loc + 1);
-	//			while (loc != string::npos) {
-	//				attempt[loc] = letter;
-	//				loc = target.find(letter, loc + 1);
-	//			}
-	//		}
-	//		cout << "You word: " << attempt << endl;
-	//		if (attempt != target)
-	//		{
-	//			if (badchars.length() > 0)
-	//				cout << "Bad choices: " << badchars << endl;
-	//			cout << guesses << " bad guesses left\n";
-	//		}
-	//	}
-	//	if (guesses > 0)
-	//		cout << "That's right!\n";
-	//	else
-	//		cout << "Sorry, the word is " << target << ".\n";
-	//	cout << "Will you play another? <y/n> ";
-	//	cin >> play;
-	//	play = tolower(play);
-	//}
+	ifstream fin;
+	fin.open("chapter16/vwords.txt");
+	vector<string> words;
+	string str;
+	if (fin.is_open())
+	{
+		while (!fin.eof())
+		{
+			fin >> str;
+			words.push_back(str);
+		}
+	}
+	fin.close();
 
-	//string path("/chapter16/random_walk.txt");
-	//ifstream fin;
-	//fin.open(path);
-	//vector<string> words;
-	//string str;
-	//if (fin.is_open())
-	//{
-	//	bool isnum = false;
-	//	while (!fin.eof())
-	//	{
-	//		fin >> str;
-	//		words.push_back(str);
-	//	}
-	//}
-	//else cout << "shs";
-	//fin.close();
-	//for (auto s : words) cout << s;
+	std::srand(std::time(0));
+	char play;
+	cout << "Will you play a word game? <y/n> ";
+	cin >> play;
+	play = tolower(play);
+	while (play == 'y')
+	{
+		string target = words[std::rand() % words.size()];
+		int length = target.length();
+		string attempt(length, '-');
+		string badchars;
+		int guesses = 6;
+		cout << "Guess my secret word. It has " << length
+			<< " letters, and you guess\none letter at time."
+			<< " You get " << guesses
+			<< " wrong guesses.\n";
+		cout << "Your word: " << attempt << endl;
+		while (guesses > 0 && attempt != target)
+		{
+			char letter;
+			cout << "Guess a letter: ";
+			cin >> letter;
+			if (badchars.find(letter) != string::npos
+				|| attempt.find(letter) != string::npos)
+			{
+				cout << "You already guessed that. Try again.\n";
+				continue;
+			}
+			int loc = target.find(letter);
+			if (loc == string::npos)
+			{
+				cout << "Oh, bad guess!\n";
+				guesses--;
+				badchars += letter;
+			}
+			else
+			{
+				cout << "Good guess!\n";
+				attempt[loc] = letter;
+				//проверить не появляется ли буква еще раз
+				loc = target.find(letter, loc + 1);
+				while (loc != string::npos) {
+					attempt[loc] = letter;
+					loc = target.find(letter, loc + 1);
+				}
+			}
+			cout << "You word: " << attempt << endl;
+			if (attempt != target)
+			{
+				if (badchars.length() > 0)
+					cout << "Bad choices: " << badchars << endl;
+				cout << guesses << " bad guesses left\n";
+			}
+		}
+		if (guesses > 0)
+			cout << "That's right!\n";
+		else
+			cout << "Sorry, the word is " << target << ".\n";
+		cout << "Will you play another? <y/n> ";
+		cin >> play;
+		play = tolower(play);
+	}
 #pragma endregion
 }
 
